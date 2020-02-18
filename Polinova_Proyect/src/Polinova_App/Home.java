97ea44575701193223b50a8742ae7e7d0c40044d
F3L1X07 <61168804+F3L1X07@users.noreply.github.com>
2020-02-17 20:11:22 -0600

    Version 1.0.0

---
+++ Polinova_Proyect/src/Polinova_App/Home.java
@@ -0,0 +1,19 @@
+/**
+	Proyecto de Innovación "SmartBelt"
+	Escuela Politecnica de Guadalajara
+	Ventana Principal (Home) de la Aplicación Movil
+*/
+package Polinova_App;
+
+public class Home {
+
+	public Home() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
