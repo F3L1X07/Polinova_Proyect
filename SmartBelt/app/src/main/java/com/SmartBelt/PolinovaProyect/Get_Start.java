2db1c24f1be0133ebe754e3ab6e220da920da034
F3L1X07 <61168804+F3L1X07@users.noreply.github.com>
2020-03-30 18:28:30 -0600

    Versión 1.0.8

--- SmartBelt/app/src/main/java/com/SmartBelt/PolinovaProyect/Get_Start.java
+++ SmartBelt/app/src/main/java/com/SmartBelt/PolinovaProyect/Get_Start.java
@@ -6,20 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Get_Start extends AppCompatActivity {
     Button GetStarted;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_getstart);
-        GetStarted = (Button)findViewById(R.id.GetStarted);
+        GetStarted = (Button)findViewById(R.id.btn_GetStarted);
+        GetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent GetStarted = new Intent(Get_Start.this, Sign_In.class);
+                startActivity(GetStarted);
+            }
+        });
+
+        /*GetStarted = (Button)findViewById(R.id.Sign_Up_access1);
         GetStarted.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent GetStarted = new Intent(Get_Start.this, Sign_Up.class);
                 startActivity(GetStarted);
             }
-        });
+        }); */
     }
 }
