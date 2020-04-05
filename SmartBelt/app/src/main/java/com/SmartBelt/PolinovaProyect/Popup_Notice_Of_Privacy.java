55b02ac0792200b4f4ee16dd418b4e47be73b21a
MitaAlba <dealba848@gmail.com>
2020-04-04 22:17:31 -0600

    Version 1.0.10

---
+++ SmartBelt/app/src/main/java/com/SmartBelt/PolinovaProyect/Popup_Notice_Of_Privacy.java
@@ -0,0 +1,14 @@
+package com.SmartBelt.PolinovaProyect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Popup_Notice_Of_Privacy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup__notice__of__privacy);
+    }
+}
