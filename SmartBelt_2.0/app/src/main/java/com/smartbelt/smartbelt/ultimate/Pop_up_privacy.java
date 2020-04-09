a5afdbd002f67e713251876de70f4b350d30e579
MitaAlba <dealba848@gmail.com>
2020-04-09 17:50:24 -0500

    Resources

---
+++ SmartBelt_2.0/app/src/main/java/com/smartbelt/smartbelt/ultimate/Pop_up_privacy.java
@@ -0,0 +1,13 @@
+package com.smartbelt.smartbelt.ultimate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pop_up_privacy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_up_privacy);
+    }
+}
