1d7c22849e31093a47856fa22520166eab378b53
F3L1X07 <61168804+F3L1X07@users.noreply.github.com>
2020-05-07 03:24:48 -0500

    Versión 1.2.6

---
+++ SmartBelt_2.0/app/src/main/java/com/smartbelt/smartbelt/ultimate/Pacient.java
@@ -0,0 +1,14 @@
+package com.smartbelt.smartbelt.ultimate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pacient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pacient_activity);
+    }
+}
