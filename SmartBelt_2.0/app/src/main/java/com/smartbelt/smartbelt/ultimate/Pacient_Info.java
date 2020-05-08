6d6432138f195535d53dae379b4f3ac1b5afdbce
F3L1X07 <61168804+F3L1X07@users.noreply.github.com>
2020-05-08 00:17:02 -0500

    Bitácora 1.2.8

---
+++ SmartBelt_2.0/app/src/main/java/com/smartbelt/smartbelt/ultimate/Pacient_Info.java
@@ -0,0 +1,14 @@
+package com.smartbelt.smartbelt.ultimate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pacient_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pacientinfo_activity);
+    }
+}
