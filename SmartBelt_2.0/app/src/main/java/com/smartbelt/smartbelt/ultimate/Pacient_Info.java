3b1153430d92e913e367306b494144d14aa3f928
F3L1X07 <61168804+F3L1X07@users.noreply.github.com>
2020-05-08 00:20:26 -0500

    Versión 3.0.0

--- SmartBelt_2.0/app/src/main/java/com/smartbelt/smartbelt/ultimate/Pacient_Info.java
+++ SmartBelt_2.0/app/src/main/java/com/smartbelt/smartbelt/ultimate/Pacient_Info.java
@@ -3,12 +3,25 @@ package com.smartbelt.smartbelt.ultimate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Pacient_Info extends AppCompatActivity {
+
+    private Button Cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pacientinfo_activity);
+
+        //Regreso al Activity anterior
+        Cancel = findViewById(R.id.btn_Cancel);
+        Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
