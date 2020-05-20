3701aead3ca4594d0d6e0cb155d8fba310ab32ce
MitaAlba <dealba848@gmail.com>
2020-05-20 01:12:57 -0500

    3.2.4

--- SmartBelt/app/src/main/java/com/smartbelt/smartbelt/ultimate/Pacient_Info.java
+++ SmartBelt/app/src/main/java/com/smartbelt/smartbelt/ultimate/Pacient_Info.java
@@ -9,11 +9,29 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Pacient_Info extends AppCompatActivity {
 
     private Button Cancel;
+
+    DatabaseReference mRootReference; //referencia a la base de datos
+    Button mButtonSubirDatosFirebase;
+    EditText mEditTextDatoName;
+    EditText mEditTextDatoLocation;
+    EditText mEditTextDatoWeight;
+    EditText mEditTextDatoHeight;
+    EditText mEditTextDatoCellphone;
+    EditText mEditTextDatoEmail;
+    //EditText mEditTextDatoBirthday;
+    EditText mEditTextDatoMedicalHistory;
+
 
     private int Dia;
     private int Mes;
@@ -57,6 +75,49 @@ public class Pacient_Info extends AppCompatActivity {
                 M_Fecha();
             }
         };
+
+        mRootReference = FirebaseDatabase.getInstance().getReference(); //inica referencia en el nodo principal.
+        mButtonSubirDatosFirebase = findViewById(R.id.btn_Accept);
+        mEditTextDatoName = findViewById(R.id.P_Name);
+        mEditTextDatoLocation = findViewById(R.id.P_Location);
+        mEditTextDatoWeight = findViewById(R.id.P_Weight);
+        mEditTextDatoHeight = findViewById(R.id.P_Height);
+        mEditTextDatoCellphone = findViewById(R.id.P_Cellphone);
+        mEditTextDatoEmail = findViewById(R.id.P_Email);
+        //mEditTextDatoBirthday = findViewById(R.id.P_Date);
+        mEditTextDatoMedicalHistory = findViewById(R.id.P_H_Medic);
+
+
+        mButtonSubirDatosFirebase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Name = mEditTextDatoName.getText().toString();
+                String Location = mEditTextDatoLocation.getText().toString();
+                int Weight = Integer.parseInt(mEditTextDatoWeight.getText().toString());
+                float Height = Float.parseFloat(mEditTextDatoHeight.getText().toString());
+                int Cellphone = Integer.parseInt(mEditTextDatoCellphone.getText().toString());
+                String Email = mEditTextDatoEmail.getText().toString();
+               // String Birthday = mEditTextDatoBirthday.getText().toString();
+                String MedicalHistory = mEditTextDatoMedicalHistory.getText().toString();
+
+                cargarDatosFirebase(Name, Location, Weight, Height, Cellphone, Email, MedicalHistory);
+            }
+        });
+
+    }// Aqui termina OnCreate
+
+    private void cargarDatosFirebase(String name, String location, int weight, float height, int cellphone, String email, String medicalHistory) {
+        Map<String, Object> datosUsuario = new HashMap<>();
+        datosUsuario.put("Name", name);
+        datosUsuario.put("Location", location);
+        datosUsuario.put("Weight", weight);
+        datosUsuario.put("Height", height);
+        datosUsuario.put("Cellphone", cellphone);
+        datosUsuario.put("Email", email);
+        //datosUsuario.put("Birthday", Birthday);
+        datosUsuario.put("MedicalHistory", medicalHistory);
+
+        mRootReference.child("Usuarios").push().setValue(datosUsuario);
     }
 
     @Override
